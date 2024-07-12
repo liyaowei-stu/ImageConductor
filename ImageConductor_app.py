74c7ffc8e7a849554e8efacac4b3050752fbcafe
Yaowei LI <58262648+liyaowei-stu@users.noreply.github.com>
2024-07-12 20:07:12 +0800

    Update ImageConductor_app.py

--- ImageConductor_app.py
+++ ImageConductor_app.py
@@ -596,7 +596,7 @@ if __name__=="__main__":
                             )
                             
                     with gr.Group():
-                        personalized = gr.Dropdown(label="Personalized", choices=["", 'HelloObject', 'TUSUN'], value="")
+                        personalized = gr.Dropdown(label="Personalized", choices=['', 'HelloObject', 'TUSUN'], value='')
                         examples_type = gr.Textbox(label="Examples Type (Ignore) ",  value="", visible=False)
 
             with gr.Column(scale=7):
